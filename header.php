4f488628c767a8a8828d64fc7a466c8ff0f180ec
francesco <francesco.viglino@webhub.it>
2025-01-29 11:24:22 +0100

    feat: push latest changes

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
-<header>
-    <nav class="header mx-auto flex  items-center p-4 bg-cs-paper text-cs-red-main" aria-label="Header">
-       <div class="flex menu-left">
+<header class="bg-cs-paper">
+    <nav class="header mx-auto flex  items-center p-4  text-cs-red-main" aria-label="Header">
+       <div class="flex menu-logo">
            <a href="/" class="">
                <img src="/src/assets/logo.svg" class="h-14 w-auto" alt="Logo" />
            </a>
@@ -23,11 +23,11 @@
                 </a>
                 <a href="" class="flex items-center menu-item relative">
                     <span class="menu-item-span">Testimonials</span>
-                    <span class="absolute bg-cs-red-main text-red-100 px-1 text-xs font-bold rounded-full -top-2 -right-8">1,252</span>
+                    <span class="testimonials-badge absolute bg-cs-red-main text-red-100 font-bold rounded-full">1,252</span>
                 </a>
         </div>
-        <div class="flex menu-right justify-end">
-            <a href="#" class="text-sm font-medium flex flex-row menu-auth-item">
+        <div class="flex menu-auth justify-end">
+            <a href="#" class="text-sm font-medium flex flex-row menu-auth-item sign-up-login-spacing">
                 <span>Sign Up</span>
                 <img src="src/assets/sign-up.svg" class="h-4 w-auto my-auto menu-auth-icon" alt="Sign Up" />
              </a>
