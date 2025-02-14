9ea6e9b3565d20886714fc8c80031b8f7f5e37ae
Mauro Iannicelli <mauro.iannicelli@gmail.com>
2025-02-14 08:45:44 +0000

    footer made my MI

--- index.php
+++ index.php
@@ -12,7 +12,20 @@
 <body class="flex flex-col min-h-screen">
     <?php require_once 'header.php'; ?>
     <main class="flex-grow">
-
+        <div class="flex flex-col items-center gap-2 mt-8 mb-32">
+            <div class="text-cs-xs">The quick brown fox jumps over the lazy dog</div>
+            <!--
+            <div class="text-cs-sm">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-base">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-lg">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-xl">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-2xl">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-3xl">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-4xl">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-5xl">The quick brown fox jumps over the lazy dog</div>
+            <div class="text-cs-6xl">The quick brown fox jumps over the lazy dog</div>
+            -->
+        </div>
     </main>
     <?php require_once 'footer.php'; ?>
 </body>
