b3adf3b166ae6e6a4e1fbb02f7e26b812adb7b3d
francesco <francesco.viglino@webhub.it>
2025-02-25 11:45:38 +0100

    vf: changes on login and added admin js

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/src/input.css" />
-    <script type="module" src="/src/main.js" defer></script>
+    <script type="module" src="/src/admin.js" defer></script>
     <title>Come & See</title>
 </head>
 <body class="flex flex-col min-h-screen">
--- login_components.php
+++ login_components.php
@@ -71,10 +71,10 @@
                         <input id="password" required type="password" placeholder="Password" class="cs-form-input" />
                         <img src="/src/assets/password-login.svg" />
                     </div>
-                    <label for="password" class="cs-form-label">Password</label>
+                    <label for="password_confirmation" class="cs-form-label">Password Confirmation</label>
                     <!-- Input Password -->
                     <div class="relative cs-form-input-container">
-                        <input id="password" required type="password" placeholder="Password confirmation"
+                        <input id="password_confirmation" required type="password" placeholder="Password confirmation"
                             class="cs-form-input" />
                     </div>
                     <button class="cs-form-submit">Reset password</button>
@@ -126,9 +126,7 @@
                     <!-- Input Phone -->
                     <div class="relative cs-form-input-container">
                         <input id="phone" required type="text" placeholder="" class="cs-form-input" />
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="absolute fill-cs-red-main stroke-cs-red-main top-4 left-5 size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                        </svg>
+                        <img src="/src/assets/phone.svg"  class="size-6" />
                     </div>
                     <button class="cs-form-submit">Submit</button>
                 </form>
