610f22c62296980e6d5d865b3b03cee9daa36773
Mauro Iannicelli <mauro.iannicelli@gmail.com>
2025-03-12 08:24:32 +0000

    Introduzione files '-test-large-menu'

--- index-test-large-menu.php
+++ index-test-large-menu.php
@@ -10,14 +10,14 @@
 </head>
 
 <body class="flex flex-col min-h-screen">
-    <?php require_once 'header-menu-test.php'; ?>
+    <?php require_once 'header-test-large-menu.php'; ?>
     <main class="flex-grow cs-main">
         <div class="flex flex-col items-center gap-2 mt-8 mb-32">
             <div class="flex flex-col pt-20">
                 <div class="cs-fs-xl max-w-[400px]">Menu Test with many menu items in header and footer.</div>
             </div>
     </main>
-    <?php require_once 'footer-menu-test.php'; ?>
+    <?php require_once 'footer-test-large-menu.php'; ?>
 </body>
 
 </html>
