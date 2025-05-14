3204469775ee15e75791f00419e4d9802b71f43a
Mauro Iannicelli <mauro.iannicelli@gmail.com>
2025-05-14 19:26:59 +0100

    small changes

--- support-us.php
+++ support-us.php
@@ -15,13 +15,13 @@
         <div class="cs-main-div">
 
             <!-- 'Title' container -->
-            <div class="relative flex flex-col items-center w-full text-cs-red-main bg-cs-paper-shade mb-24 md:mb-48">
+            <div class="relative flex flex-col items-center w-full mb-24 text-cs-red-main bg-cs-paper-shade md:mb-48">
                 <video autoplay loop muted playsinline class="w-auto min-w-full">
                     <source src="/src/assets/videos/bg-video-support-us.mp4" type="video/mp4" />
                 </video>
                 <div class="absolute bottom-0 left-0 flex items-center justify-center w-full h-full px-4 bg-white bg-opacity-30">
                 </div>
-                <div class="w-full max-w-4xl absolute bottom-0 flex items-center mx-auto">
+                <div class="absolute bottom-0 flex items-center w-full max-w-4xl mx-auto">
                     <div class="-mb-20 sm:-mb-24 md:-mb-36">
                         <img src="/src/assets/support-us-header.png" class="md:w-[700px] sm:w-[500px] w-[400px] mr-auto" alt="Header">
                     </div>
@@ -30,16 +30,17 @@
 
             <div class="flex flex-col items-center cs-personal-area-container sm:cs-fs-base cs-fs-base">
 
-                <div class="flex flex-col justify-start gap-5 mt-8 mb-10 cs-hero-support text-cs-grey">
+                <div class="flex flex-col justify-start gap-2 mt-8 text-cs-grey">
 
-                    <p class="uppercase font-bold cs-fs-lg">We’d love to have YOU as our mission partner!</p>
+                    <p class="font-bold uppercase cs-fs-lg">We'd love to have YOU as our mission partner!</p>
 
                     <p>We are delighted to offer you our courses, LIVE, totally <b>FREE OF CHARGE!</b></p>
 
                     <p>However, my wife Janet and I would be so blessed <b>if you could help us financially by buying our Study Resources!</b></p>
+                </div>
 
+                <div class="w-full mt-16 mb-10">
                     <h3 class="font-bold cs-fs-xl sm:cs-fs-2xl text-cs-red-main">Bible Study Resource Bundles</h3>
-
                 </div>
 
                 <div class="cs-support-us-box bg-cs-bronze">
@@ -65,10 +66,15 @@
 
                             </div>
                             <div class="cs-support-us-item">
-                                <span class="inline-block font-sans font-normal">
-                                    These notes cover the whole course, <b>from Period 1 to Period 12</b> and provide you with a convenient summary of Salvation History.<br><br>
-                                    You can read from the <a class="underline" href="#">testimonials</a> how the students of past years found these course notes super useful!
-                                </span>
+                                <div class="cs-support-us-paragraphs-block">
+                                    <p>
+                                        These notes cover the whole course, <b>from Period 1 to Period 12</b> and provide you with a convenient summary of Salvation History.
+                                    </p>
+                                    <p>
+                                        You can read from the <a class="underline" href="#">testimonials</a> how the students of past years found these course notes super useful!
+                                        font-sans font-normal
+                                    </p>
+                                </div>
                             </div>
                             <div class="cs-support-us-item">
                                 <button class="cs-support-us-button">Buy</button>
@@ -82,7 +88,7 @@
                         <span class="bg-cs-silver-light cs-support-us-tag">
                             Silver Bundle
                         </span>
-                        $50
+                        £50
                     </button>
                     <div class="bg-cs-paper-dark">
                         <div class="flex flex-col py-6 cs-bible-accordion-content">
@@ -109,10 +115,14 @@
                                 </a>
                             </div>
                             <div class="cs-support-us-item">
-                                <span class="inline-block font-sans font-normal">
-                                    Study at your own pace! You will be able to watch the recording of ANY course session whenever you want with <b>unlimited lifetime access!</b><br><br>
-                                    <b>Please Note:</b> Due to Copyright issues, you agree not to share these videos with anyone else.
-                                </span>
+                                <div class="cs-support-us-paragraphs-block">
+                                    <p>
+                                        Study at your own pace! You will be able to watch the recording of ANY course session whenever you want with <b>unlimited lifetime access!</b>
+                                    </p>
+                                    <p>
+                                        <b>Please Note:</b> Due to Copyright issues, you agree not to share these videos with anyone else.
+                                    </p>
+                                </div>
                             </div>
                             <div class="cs-support-us-item">
                                 <button class="cs-support-us-button">Buy</button>
@@ -169,10 +179,14 @@
                                 </a>
                             </div>
                             <div class="cs-support-us-item">
-                                <span class="inline-block font-sans font-normal">
-                                    As a Come & See Mission Partner, you are welcome to join our <b>Strategy Planning Zoom Calls</b>, where we will discuss and discern together the vision and the next steps of this ministry.<br><br>
-                                    Moreover, as a Mission Partner, Janet and I will lift up your name in our prayers for the next 12 months - <b>we will honour this!</b>
-                                </span>
+                                <div class="cs-support-us-paragraphs-block">
+                                    <p>
+                                        As a Come & See Mission Partner, you are welcome to join our <b>Strategy Planning Zoom Calls</b>, where we will discuss and discern together the vision and the next steps of this ministry.
+                                    </p>
+                                    <p>
+                                        Moreover, as a Mission Partner, Janet and I will lift up your name in our prayers for the next 12 months - <b>we will honour this!</b>
+                                    </p>
+                                </div>
                             </div>
                             <div class="cs-support-us-item">
                                 <button class="cs-support-us-button">Buy</button>
@@ -180,10 +194,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="cs-support-us-box text-black">
+                <div class="text-black cs-support-us-box">
                     <div class="bg-cs-paper-dark">
-                        <div class="flex flex-col items-center py-6 cs-bible-accordion-content">
-                            <h3 class="font-bold text-center text-cs-red-main cs-fs-xl max-w-96">Or donate any amount of your choice!
+                        <div class="flex flex-col items-center py-10 cs-bible-accordion-content">
+                            <h3 class="font-bold text-center text-cs-red-main cs-fs-xl max-w-96">Or donate any amount of&nbsp;your&nbsp;choice!
                             </h3>
                             <div class="justify-center inline-block mt-3 font-sans font-normal text-center cs-support-us-item">
                                 Please Note: if you donate more than £10, £50 or £100 respectively, you will of course be <b>automatically entitled</b> to the resources as stated for the amounts above.
@@ -197,7 +211,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="mb-14 flex flex-col items-center">
+                <div class="flex flex-col items-center mb-14">
                     <img src="/src/assets/payments-icons.png" class="h-16" alt="Payment Methods">
                     <p class="italic text-center"><b>PLEASE NOTE:</b> below, payments can be made by Credit/Debit Card. Alternatively, you can ask for our <a class="font-bold underline " href="#">Bank Details via email.</a></p>
                 </div>
@@ -206,22 +220,22 @@
             <div class="flex flex-col items-center w-full p-5 sm:p-10 bg-cs-paper-dark">
                 <div class="w-full max-w-[1000px] my-5 flex flex-col gap-3">
                     <span class="tracking-widest uppercase sm:cs-fs-xs cs-fs-xs text-cs-grey">pray before giving</span>
-                    <h2 class=" cs-fs-xl sm:cs-fs-2xl text-cs-red-main font-bold">We invite you to pray before you decide what to give.</h2>
+                    <h2 class="font-bold cs-fs-xl sm:cs-fs-2xl text-cs-red-main">We invite you to pray before you decide what to give.</h2>
                     <span class="tracking-wide text-black sm:cs-fs-base cs-fs-base">We would absolutely love it if you took a decision based on prayer! We are doing a Bible study together, so - <b>involve God!</b></span>
                     <span class="tracking-wide sm:cs-fs-base cs-fs-base">So… may we suggest you do one thing?</span>
                     <div class="cs-support-us-steps">
                         <div class="cs-support-us-step">
-                            <div class="flex flex-row gap-2 items-baseline">
+                            <div class="flex flex-row items-baseline gap-2">
                                 <span class="text-white bg-cs-red-main border-cs-red-main border rounded-full w-8 px-2 font-serif font-bold cs-fs-2md flex-[0_0_2rem]">1</span>
-                                <span class="font-normal cs-fs-base md:cs-fs-base text-left">Find a convenient moment to enter into a <b>time of prayer</b>.</span>
+                                <span class="font-normal text-left cs-fs-base md:cs-fs-base">Find a convenient moment to enter into a <b>time of prayer</b>.</span>
                             </div>
-                            <div class="flex flex-row gap-2 items-baseline">
+                            <div class="flex flex-row items-baseline gap-2">
                                 <span class="text-white bg-cs-red-main border-cs-red-main border rounded-full w-8 px-2 font-serif font-bold cs-fs-2md flex-[0_0_2rem]">2</span>
-                                <span class="font-normal cs-fs-base md:cs-fs-base text-left">Ask the Lord to <b>put the figure into your heart</b> that He desires you to give.</span>
+                                <span class="font-normal text-left cs-fs-base md:cs-fs-base">Ask the Lord to <b>put the figure into your heart</b> that He desires you to give.</span>
                             </div>
-                            <div class="flex flex-row gap-2 items-baseline">
+                            <div class="flex flex-row items-baseline gap-2">
                                 <span class="text-white bg-cs-red-main border-cs-red-main border rounded-full w-8 px-2 font-serif font-bold cs-fs-2md flex-[0_0_2rem]">3</span>
-                                <span class="font-normal cs-fs-base md:cs-fs-base text-left">Then, <b>honour that prompting</b>— knowing that the Lord will bless you abundantly, as He is never outdone in generosity!</span>
+                                <span class="font-normal text-left cs-fs-base md:cs-fs-base">Then, <b>honour that prompting</b>— knowing that the Lord will bless you abundantly, as He is never outdone in generosity!</span>
                             </div>
                         </div>
                     </div>
