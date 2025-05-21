29b2c68a59ef2847a0b5fc4297b498283d695a3a
francesco <francesco.viglino@webhub.it>
2025-05-21 13:21:37 +0200

    fix: fixed testimonials and some svg positioning

--- bible-timeline.php
+++ bible-timeline.php
@@ -109,8 +109,8 @@
                     <b>there is a<br/> definitive solution</b>
                     to this<br/> problem?
                 </h2>
-                <div class="bg-contain bg-[url(/src/assets/bible-timeline/trail.svg)] pt-[36%] bg-no-repeat bg-[4rem]">
-                    <p class="cs-fs-lg text-left -mt-4 w-[50%]">
+                <div class="bg-contain bg-[url(/src/assets/bible-timeline/trail.svg)] pt-[30%] md:pt-[36%] bg-no-repeat bg-[0rem_0rem] md:bg-[4rem]">
+                    <p class="cs-fs-lg text-left -mt-4 w-[70%] md:w-[50%]">
                         The Bible Timeline methodology breaks the Bible into <b>12 colour-coded time periods</b> that are easy to understand and memorize.
                     </p>
                 </div>
@@ -173,8 +173,8 @@
         <div class=" flex flex-col items-center w-full p-5 sm:p-10 bg-cs-paper">
             <div class="relative w-full max-w-[1000px] mt-48 mb-5 flex flex-col gap-6 text-center items-center">
                 <img src="/src/assets/bible-timeline/vector-27.svg" class="absolute left-10 md:left-1/4 -top-64 w-72 md:w-96">
-                <h2 class="relative font-sans font-bold cs-fs-xl md:cs-fs-3xl sm:cs-fs-2xl cs-gradient-text mt-4">We make the complex <br>simple.
-                    <img src="/src/assets/bible-timeline/im-in.svg" class="absolute -right-4 w-48 top-20">
+                <h2 class="relative font-sans font-bold cs-fs-xl md:cs-fs-3xl sm:cs-fs-2xl cs-gradient-text -mt-10 md:mt-4">We make the complex <br>simple.
+                    <img src="/src/assets/bible-timeline/im-in.svg" class="absolute -right-4 w-40 md:w-48 top-36 md:top-20">
                 </h2>
                 <p class="cs-fs-lg w-full text-center">
                     Join us online. <b>Come and see!</b>
@@ -221,95 +221,70 @@
 
         <!-- Boxes Testimonial Wrapper -->
         <div class="flex flex-col items-center w-full p-5 sm:p-10 bg-cs-paper my-10">
-        <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 row-auto gap-5 max-w-[1000px]">
-
-            <!-- Box Testimonial -->
-            <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-medium">
-                <!-- Box Author -->
-                <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-medium pl-[20px]">
-                    <div class="flex flex-row items-center justify-start gap-2 -mt-1">
-                        <div class="cs-testimonials-counter text-cs-red-medium"># 1,252</div>
-                        <span class="font-bold text-white">Anne B.</span>
+        <div class="flex flex-col md:flex-row gap-4 md:gap-12 max-w-[1000px]">
+            <div class="flex flex-col md:w-1/2 w-full gap-4">
+                <!-- Box Testimonial -->
+                <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-dark">
+                    <!-- Box Author -->
+                    <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-dark pl-[20px]">
+                        <div class="flex flex-row items-center justify-start gap-2 -mt-1">
+                            <span class="font-bold text-white">Helen B.</span>
+                        </div>
+                        <p class="tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
                     </div>
-                    <p class="mt-1 tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
+                    <!-- Box Post -->
+                    <div class="flex flex-col gap-5 px-8 py-8">
+                        <p>I feel completely blessed by the Advent Bible Series (as I have by all the courses I have taken with comeandsee.org to be honest), it has brought the scripture to life for me and helped me to see things in a totally new way.</p>
+                        <p>I love that the sessions are live and that I am taking part with others from all over the world at the same time - it gives me such a feeling of fellowship.</p>
+                    </div>
                 </div>
-                <!-- Box Post -->
-                <div class="flex flex-col gap-5 px-8 py-8">
-                    <p>The depth Mauro goes to to open the Sunday Gospel is astounding. Mauro's teaching is 100% Catholic, and will bring something new to everyone, whatever their level of understanding of the Faith and their knowledge of the Scripture.</p>
 
-                    <p>We met as a little group in the church hall, watched together and stayed all behind after the session to discuss the questions. This was the best way to exchange, share and grow together in the Holy Spirit. Only two of us could be at the hall for the last session, but we met the others on Zoom, and could still have our discussion group going.</p>
 
-                    <p>Our bonds of friendship are strengthened, and I feel Mauro and Janet's mission to serve the Church is spot on.</p>
-
-                    <p>Presenting the course online must have been a challenge, and I know you will both get better and better.</p>
-
-                    <p>Thank you so much to you, Mauro and Janet, for stimulating my brain and my heart, and bringing me closer to God. All my prayers go to you.</p>
+                <!-- Box Testimonial -->
+                <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-bright">
+                    <!-- Box Author -->
+                    <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-bright pl-[20px]">
+                        <div class="flex flex-row items-center justify-start gap-2 -mt-1">
+                            <span class="font-bold text-white">Marie D.</span>
+                        </div>
+                        <p class="tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
+                    </div>
+                    <!-- Box Post -->
+                    <div class="flex flex-col gap-5 px-8 py-8">
+                        <p>This course has been such a blessing from God in my life! Each and every session has been jam packed with in depth teachings that have taught me more than I could ever imagine!</p>
+                    </div>
                 </div>
+
             </div>
-
-            <!-- Box Testimonial -->
-            <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-main">
-                <!-- Box Author -->
-                <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-main pl-[20px]">
-                    <div class="flex flex-row items-center justify-start gap-2 -mt-1">
-                        <div class="cs-testimonials-counter border-cs-red-main text-cs-red-main"># 1,251</div>
-                        <span class="font-bold text-white">Anita P.</span>
+            <div class="flex flex-col md:w-1/2 w-full gap-4">
+                <!-- Box Testimonial -->
+                <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-medium">
+                    <!-- Box Author -->
+                    <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-medium pl-[20px]">
+                        <div class="flex flex-row items-center justify-start gap-2 -mt-1">
+                            <span class="font-bold text-white">Anne B.</span>
+                        </div>
+                        <p class="mt-1 tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
                     </div>
-                    <p class="mt-1 tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
-                </div>
-                <!-- Box Post -->
-                <div class="flex flex-col gap-5 px-8 py-8">
-                    <p>Just SO surprisingly interesting and informative! Mauro's infectious enthusiasm and his analysis interspersed with interesting historical facts and (often surprising) connections within the Bible, lures and retains one's attention.</p>
-
-                    <p>These are such good courses and and what at first sight seems like a rather dry proposition (reflecting on a bible reading for an hour) is fascinating.</p>
-
-                    <p>Mauro always manages to enthuse his listeners/course participants! Even friends who had left the faith, willingly tuned in each week - after attending the first session!</p>
-                </div>
-            </div>
-
-            <!-- Box Testimonial -->
-            <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-dark">
-                <!-- Box Author -->
-                <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-dark pl-[20px]">
-                    <div class="flex flex-row items-center justify-start gap-2 -mt-1">
-                        <div class="cs-testimonials-counter border-cs-red-dark text-cs-red-dark"># 1,250</div>
-                        <span class="font-bold text-white">Helen B.</span>
+                    <!-- Box Post -->
+                    <div class="flex flex-col gap-5 px-8 py-8">
+                        <p>The depth Mauro goes to to open the Sunday Gospel is astounding. Mauro's teaching is 100% Catholic, and will bring something new to everyone, whatever their level of understanding of the Faith and their knowledge of the Scripture.</p>
                     </div>
-                    <p class="tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
                 </div>
-                <!-- Box Post -->
-                <div class="flex flex-col gap-5 px-8 py-8">
-                    <p>I feel completely blessed by the Advent Bible Series (as I have by all the courses I have taken with comeandsee.org to be honest), it has brought the scripture to life for me and helped me to see things in a totally new way.</p>
 
-                    <p>I love that the sessions are live and that I am taking part with others from all over the world at the same time - it gives me such a feeling of fellowship.</p>
-
-                    <p>Mauro's teaching is inspired and has helped me draw closer to God and to grow in faith since my recent return to the Catholic Church after an absence of many years and I would recommend it to anyone at any stage on their journey of faith.</p>
-
-                    <p>It was lovely to 'meet' Janet too - thank you for all you do behind the scenes and for your wonderful prayer. I am sure that the Holy Spirit is greatly at work through Mauro, Janet and comeandsee.org - what a blessing!</p>
-                </div>
-            </div>
-
-            <!-- Box Testimonial -->
-            <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-bright">
-                <!-- Box Author -->
-                <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-bright pl-[20px]">
-                    <div class="flex flex-row items-center justify-start gap-2 -mt-1">
-                        <div class="cs-testimonials-counter border-cs-red-bright text-cs-red-bright"># 1,249</div>
-                        <span class="font-bold text-white">Marie D.</span>
+                <!-- Box Testimonial -->
+                <div class="flex flex-col items-center w-full cs-testimonials-box border-cs-red-main">
+                    <!-- Box Author -->
+                    <div class="relative flex flex-col justify-center cs-testimonials-box-author bg-cs-red-main pl-[20px]">
+                        <div class="flex flex-row items-center justify-start gap-2 -mt-1">
+                            <span class="font-bold text-white">Anita P.</span>
+                        </div>
+                        <p class="mt-1 tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
                     </div>
-                    <p class="tracking-widest text-white uppercase cs-fs-2xs">Advent Sunday Gospels - Nov 2021</p>
-                </div>
-                <!-- Box Post -->
-                <div class="flex flex-col gap-5 px-8 py-8">
-                    <p>This course has been such a blessing from God in my life! Each and every session has been jam packed with in depth teachings that have taught me more than I could ever imagine!</p>
-
-                    <p>The study of the Bible passages is so so in depth and interesting and it is so evident how much love, care, prayer and time Mauro puts into each session! It has been the highlight of my week throughout advent!</p>
-
-                    <p>It has made it even more exiting to go to Sunday mass having been able to know detailed information about the Gospels!</p>
-
-                    <p>Each week I have been blessed with more and more teachings and it has really helped me grow in my spiritual life and in my knowledge and understanding of the Gospel!</p>
-
-                    <p>It has been such a joy to participate Thank you so much God and Mauro and Janet for making this happen!</p>
+                    <!-- Box Post -->
+                    <div class="flex flex-col gap-5 px-8 py-8">
+                        <p>Just SO surprisingly interesting and informative! Mauro's infectious enthusiasm and his analysis interspersed with interesting historical facts and (often surprising) connections within the Bible, lures and retains one's attention.</p>
+                    </div>
                 </div>
             </div>
         </div>
